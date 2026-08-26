7df7ccfc36a66aa59c1fd913df431850131f7d75
codeforge11 <codeforge11@gmail.com>
2026-08-26 15:59:11 +0200

    refactor: update log messages for consistency in English

--- main.js
+++ main.js
@@ -4,16 +4,17 @@ const { Plugin, Notice, apiVersion, requireApiVersion } = require('obsidian');
 /** @extends Plugin */
 module.exports = class JustFTablesPlugin extends Plugin {
     async onload() {
-        console.log('Wtyczka JustFTables załadowana!');
+        console.log('JustFTables, trun on!');
         
         this.addCommand({
             id: 'testowa-komenda',
             name: 'Show version',
             callback: () => new Notice('JustFTables version:'+this.manifest.version)
         });
+
     }
 
     onunload() {
-        console.log('Wtyczka JustFTables wyłączona.');
+        console.log('JustFTables, turn off!');
     }
 };
